d65c3ca1e6d618109fdb59d021649dca671d37e4
dev-kb <kb@dev>
2014-01-12 14:52:33 -0800

    Update EF5->EF6, spec. System.Data -> System.Data.Entity

--- TodoApp/Models/TaskRepository.cs
+++ TodoApp/Models/TaskRepository.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
-
 namespace TodoApp.Models
-{ 
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Linq.Expressions;
+
     public class TaskRepository : ITaskRepository
     {
         TodoAppContext context = new TodoAppContext();
--- TodoApp/Models/TodoAppContext.cs
+++ TodoApp/Models/TodoAppContext.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
-
 namespace TodoApp.Models
 {
+    using System.Data.Entity;
+
     public class TodoAppContext : DbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
