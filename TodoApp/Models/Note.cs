df38c9963d7e11fe5553e38ec2317e479d53b60e
dev-kb <kb@dev>
2014-01-12 14:41:39 -0800

    Add models

---
+++ TodoApp/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoApp.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+        public string Description { get; set; }
+        public int? TaskId { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ TodoApp/Models/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Status
+    {
+        [Key]
+        public int StatusId { get; set; }
+        public string Name { get; set; }
+    }
+}
